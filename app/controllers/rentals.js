116f1b6508cf963d682b5a6c4e7ab654dafdc941
Michael Alvarez <michaelalvarez@Michaels-MacBook-Pro-3.local>
2017-02-23 21:16:47 -0600

    can access models from any view

--- app/controllers/rentals.js
+++ app/controllers/rentals.js
@@ -3,16 +3,21 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   actions: {
-
       saveRental: function() {
           var _that = this;
-          var email = this.get('emailAddress');
+          var _name = this.get('name');
+          var _units = this.get('units');
 
           var newRental = this.store.createRecord('rental', {
-              name: 'test rental firebase',
-              units: 40
+              name: _name,
+              units: _units
           });
-
+          var newSomething = this.store.createRecord('something', {
+              name: _name,
+              units: _units
+          });
+          newSomething.save();
+          
           newRental.save().then(function(response) {
               _that.set('responseMessage', "Thank you! We saved your email address with the following id: " + response.get('id'));
               _that.set('emailAddress', '');
---
+++ app/models/something.js
@@ -0,0 +1,9 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  name: DS.attr('string'),
+  bedrooms: DS.attr('number'),
+  bathrooms: DS.attr('number'),
+  squarefoot: DS.attr('number'),
+  units: DS.attr('number')
+});
