3a016c67b16c8f83b180bb1606ae9ab260639ae9
Michael Alvarez <michaelalvarez@Michaels-MacBook-Pro-3.local>
2017-02-25 15:03:13 -0600

    can now go to update rental page

--- app/routes/update-rental.js
+++ app/routes/update-rental.js
@@ -2,8 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(params) {
-    // var thisProperty = this.store.find('rental', { name: "Peter" });
     console.log('the rental id is ' + params.rental_id);
-    // return thisProperty;
+
+    return this.store.findRecord('rental', params.rental_id);
   }
 });
